18aa1f98148a756a168bfe2c7e7871fbd41a71f0
Q-I-U <qiu@qiu.news>
2020-07-19 10:40:02 +0200

    Added a another status message and fixed the the other random elemant picker

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import os
 import random
 import pygame_sdl2 as pygame
 import sys
+import getch
 
 pygame.mixer.init()
 path = os.environ['HOME'] +'/music'
@@ -17,37 +18,33 @@ def music_load(path):
 
 
 music = music_load(path)
-choice_music = random.randrange(1, len(music))
+choice_music = random.randrange(0, len(music))
 pygame.mixer.music.load(music[choice_music])
 
+print("Random music - 1, Next song - 2, Stop - 3, resume - 4, Mute - 5, Unmute - 6, Exit - 7: \n")
 while(choice != 0):
-	print("Random music - 1, Next song - 2, Previous song - 3, Stop - 4, resume - 5, Mute - 6, Unmute - 7, Exit - 8: \n")
-	print("Enter your choice: ")
-	choice = input(">")
+	choice = getch.getch()
 	if choice == '1':
-                filename = random.choice(music)
-                pygame.mixer.music.load(filename)
-                pygame.mixer.music.play()
+		filename = random.choice(music)
+		pygame.mixer.music.load(filename)
+		pygame.mixer.music.play()
+		print("Playing:\n" + filename)
 	elif choice == '2':
 		choice_music = random.randrange(0, len(music))
 		pygame.mixer.music.load(music[choice_music])
 		pygame.mixer.music.play()
+		print("Playing:\n" + filename)
 	elif choice == '3':
-		choice_music -= 1
-		choice_music = random.randrange(0, len(music))
-		pygame.mixer.music.load(music[choice_music])
-		pygame.mixer.music.play()
-	elif choice == '4':
 		pygame.mixer.music.pause()
 		print("Music is paused")
-	elif choice == '5':
+	elif choice == '4':
 		pygame.mixer.music.unpause()
 		print("Music is unpaused")
-	elif choice == '6':
+	elif choice == '5':
 		pygame.mixer.music.set_volume(0.0)
-	elif choice == '7':
+	elif choice == '6':
 		pygame.mixer.music.set_volume(1.0)
-	elif choice == '8':
+	elif choice == '7':
 		sys.exit()
 	#elif choice == '9':	
 		#Autoplay
