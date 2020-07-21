de6fc10245f5a1a5c4f3aa31e1abfd773c3995f0
Q-I-U <qiu@qiu.news>
2020-07-21 10:04:35 +0200

    Added autoplay using multiprocessing I spilt the program into two processes: - The first process is listening for user input - The second process is checking what the input was and executing the corresponding commands. Aditonally the second process is alsways checking if 'Autoplay' is set to 'True'. If it is then the process is automaticly starting to play music.

--- player.py
+++ player.py
@@ -3,11 +3,11 @@ import random
 import pygame_sdl2 as pygame
 import sys
 import getch
+import multiprocessing
+import time
 
 pygame.mixer.init()
 path = os.environ['HOME'] +'/music'
-
-choice = 1
 
 def music_load(path):
 	music = []
@@ -20,26 +20,48 @@ def music_load(path):
 music = music_load(path)
 choice_music = random.randrange(0, len(music))
 pygame.mixer.music.load(music[choice_music])
+autoplay = False
+filename = "none"
+choice = multiprocessing.Value('i', 0)
 
-print("Random music - 1, Stop - 2, resume - 3, Mute - 4, Unmute - 5, Exit - 6: \n")
-while(choice != 0):
-	choice = getch.getch()
-	if choice == '1':
+def loop(cho):
+	while(True):
+		char = getch.getch()
+		cho.value = int(char)
+
+p = multiprocessing.Process(target=loop, args=(choice,))
+p.start()
+
+print("Random music - 1, Stop - 2, resume - 3, Mute - 4, Unmute - 5, Autoplay - 6, Exit - 7\n")
+	
+while(True):
+	ch = choice.value
+	if ch == 1:
 		filename = random.choice(music)
 		pygame.mixer.music.load(filename)
 		pygame.mixer.music.play()
 		print("Playing:\n" + filename)
-	elif choice == '2':
+	elif ch == 2:
 		pygame.mixer.music.pause()
 		print("Music is paused")
-	elif choice == '3':
+	elif ch == 3:
 		pygame.mixer.music.unpause()
 		print("Music is unpaused")
-	elif choice == '4':
+	elif ch == 4:
 		pygame.mixer.music.set_volume(0.0)
-	elif choice == '5':
+		print("Music is muted")
+	elif ch == 5:
 		pygame.mixer.music.set_volume(1.0)
-	elif choice == '6':
+		print("Music is unmuted")
+	elif ch == 6:
+		autoplay = not autoplay
+		print("Autoplay set to " + repr(autoplay))
+	elif ch == 7:	
 		sys.exit()
-	#elif choice == '7':	
-		#Autoplay
+	if autoplay == True and pygame.mixer.music.get_busy() == 0:
+		filename = random.choice(music)
+		pygame.mixer.music.load(filename)
+		pygame.mixer.music.play()
+		print("Playing:\n" + filename)
+	choice.value = 0
+	time.sleep(0.3)
