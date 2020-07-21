4f4b0bbd3e1f0cf70f42aaa499fa939631539e1f
Q-I-U <qiu@qiu.news>
2020-07-21 11:47:30 +0200

    Added a better status mechanism Thanks to the new multiprocess aproach it's is possible to add better status messages/bar

--- player.py
+++ player.py
@@ -22,7 +22,11 @@ choice_music = random.randrange(0, len(music))
 pygame.mixer.music.load(music[choice_music])
 autoplay = False
 filename = "none"
+oldfilename = "none"
 choice = multiprocessing.Value('i', 0)
+muted = "No"
+paused = "No"
+status = ""
 
 def loop(cho):
 	while(True):
@@ -32,36 +36,49 @@ def loop(cho):
 p = multiprocessing.Process(target=loop, args=(choice,))
 p.start()
 
-print("Random music - 1, Stop - 2, resume - 3, Mute - 4, Unmute - 5, Autoplay - 6, Exit - 7\n")
+print("Random music - 1, Stop - 2, resume - 3, Mute - 4, Unmute - 5, Autoplay - 6, Exit - 7\n\n")
 	
 while(True):
 	ch = choice.value
 	if ch == 1:
+		sys.stdout.write('\r'+ (20 + len(oldfilename)) * " ")
+		oldfilename = filename
 		filename = random.choice(music)
 		pygame.mixer.music.load(filename)
 		pygame.mixer.music.play()
-		print("Playing:\n" + filename)
+		paused = "No"
 	elif ch == 2:
 		pygame.mixer.music.pause()
-		print("Music is paused")
+		paused = "Yes"
 	elif ch == 3:
 		pygame.mixer.music.unpause()
-		print("Music is unpaused")
+		paused = "No"
 	elif ch == 4:
 		pygame.mixer.music.set_volume(0.0)
-		print("Music is muted")
+		muted = "Yes"
 	elif ch == 5:
 		pygame.mixer.music.set_volume(1.0)
-		print("Music is unmuted")
+		muted = "No"
 	elif ch == 6:
 		autoplay = not autoplay
-		print("Autoplay set to " + repr(autoplay))
 	elif ch == 7:	
-		sys.exit()
+		print("\nExiting...\n")
+		p.terminate()
+		os._exit(0)
+		quit()
 	if autoplay == True and pygame.mixer.music.get_busy() == 0:
+		sys.stdout.write('\r'+ (20 + len(oldfilename)) * " ")
+		oldfilename = filename
 		filename = random.choice(music)
 		pygame.mixer.music.load(filename)
 		pygame.mixer.music.play()
-		print("Playing:\n" + filename)
+		paused = "No"
+
+	print("\033[F" + 36 * " ")
+	print("\033[FMuted=" + muted + " Paused=" + paused + " Autoplay=" + repr(autoplay))
+	# sys.stdout.write('\r'+ 10 * " ")
+	status = "Playing: " + os.path.basename(filename)
+	sys.stdout.write('\r'+ (5 + len(filename)) * " ")
+	sys.stdout.write('\r'+status)
 	choice.value = 0
 	time.sleep(0.3)
