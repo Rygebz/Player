acac88e4d96cfd6fcbf21d19bf639ed52a2d72f6
Patro-ss <68190141+Patro-ss@users.noreply.github.com>
2020-07-31 19:46:44 +0200

    Medium update

--- player.py
+++ player.py
@@ -7,7 +7,9 @@ import multiprocessing
 import time
 
 pygame.mixer.init()
-path = os.environ['HOME'] +'/music'
+path = os.environ['HOME'] +'/Music'
+
+choice = 1
 
 def music_load(path):
 	music = []
@@ -16,17 +18,12 @@ def music_load(path):
 			music.append(os.path.join(path, filename))
 	return music
 
-
 music = music_load(path)
 choice_music = random.randrange(0, len(music))
 pygame.mixer.music.load(music[choice_music])
 autoplay = False
 filename = "none"
-oldfilename = "none"
 choice = multiprocessing.Value('i', 0)
-muted = "No"
-paused = "No"
-status = ""
 
 def loop(cho):
 	while(True):
@@ -36,49 +33,39 @@ def loop(cho):
 p = multiprocessing.Process(target=loop, args=(choice,))
 p.start()
 
-print("Random music - 1, Stop - 2, resume - 3, Mute - 4, Unmute - 5, Autoplay - 6, Exit - 7\n\n")
-	
-while(True):
+print("\nRandom music - 1, \nStop - 2, \nresume - 3, \nMute - 4, \nUnmute - 5, \nAutoplay - 6, \nExit - 7\n")
+
+while True:
 	ch = choice.value
 	if ch == 1:
-		sys.stdout.write('\r'+ (20 + len(oldfilename)) * " ")
-		oldfilename = filename
 		filename = random.choice(music)
 		pygame.mixer.music.load(filename)
 		pygame.mixer.music.play()
-		paused = "No"
+		print("Playing:\n " + os.path.basename(filename))
 	elif ch == 2:
 		pygame.mixer.music.pause()
-		paused = "Yes"
+		print("Music is paused")
 	elif ch == 3:
 		pygame.mixer.music.unpause()
-		paused = "No"
+		print("Music is unpaused")
 	elif ch == 4:
 		pygame.mixer.music.set_volume(0.0)
-		muted = "Yes"
+		print("Music is muted")
 	elif ch == 5:
 		pygame.mixer.music.set_volume(1.0)
-		muted = "No"
+		print("Music is unmuted")
 	elif ch == 6:
 		autoplay = not autoplay
+		print("Autoplay set to " + repr(autoplay))
 	elif ch == 7:	
 		print("\nExiting...\n")
 		p.terminate()
 		os._exit(0)
 		quit()
 	if autoplay == True and pygame.mixer.music.get_busy() == 0:
-		sys.stdout.write('\r'+ (20 + len(oldfilename)) * " ")
-		oldfilename = filename
 		filename = random.choice(music)
 		pygame.mixer.music.load(filename)
 		pygame.mixer.music.play()
-		paused = "No"
-
-	print("\033[F" + 36 * " ")
-	print("\033[FMuted=" + muted + " Paused=" + paused + " Autoplay=" + repr(autoplay))
-	# sys.stdout.write('\r'+ 10 * " ")
-	status = "Playing: " + os.path.basename(filename)
-	sys.stdout.write('\r'+ (5 + len(filename)) * " ")
-	sys.stdout.write('\r'+status)
+		print("Playing:\n" + os.path.basename(filename))
 	choice.value = 0
 	time.sleep(0.3)
